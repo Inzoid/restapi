7064fcfb105d13abe4c0c2369cf7ede94f5cb337
inzoid <inzoid21@gmail.com>
2019-11-12 16:06:28 +0700

    rest api with jwt

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middeware' => 'api', 'prefix' => 'auth',], function
+($router) {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('me', 'AuthController@me');
+    Route::put('update_name', 'AuthController@updateName');
+
+});
